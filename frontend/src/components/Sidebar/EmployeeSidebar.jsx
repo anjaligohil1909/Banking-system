04f41cdcada400b230d52f9be5e77aa7d9542e5d
Anjali Gohil <anjali.gohil@gmail.com>
2024-05-03 00:59:00 -0400

    Routes Outlet Implemented SideBar Dynamic List New-Account Page Created

--- frontend/src/components/Sidebar/EmployeeSidebar.jsx
+++ frontend/src/components/Sidebar/EmployeeSidebar.jsx
@@ -1,19 +1,21 @@
 import * as React from "react";
 import Box from "@mui/material/Box";
 import Drawer from "@mui/material/Drawer";
+import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 import List from "@mui/material/List";
+import Typography from "@mui/material/Typography";
+import Divider from "@mui/material/Divider";
 import ListItem from "@mui/material/ListItem";
 import ListItemButton from "@mui/material/ListItemButton";
 import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
 import InboxIcon from "@mui/icons-material/MoveToInbox";
 import MailIcon from "@mui/icons-material/Mail";
-import {Link} from "react-router-dom";
+
 const drawerWidth = 240;
 
-export default function Sidebar(props) {
-  const sideBarList = props.sideBarList;
+export default function EmployeeSidebar() {
   return (
     <Drawer
       PaperProps={{
@@ -38,17 +40,16 @@ export default function Sidebar(props) {
       {/* <Divider /> */}
       <Box sx={{ overflow: "auto", mt: 6, mx: 3}}>
         <List>
-          {sideBarList != null ? Object.keys(sideBarList).map((key, index) => (
-             <ListItem key={key} disablePadding sx={{ my: 2 }} button component={Link} to={sideBarList[key]}>
-                <ListItemButton>
+          {["New Account", "Customer List", "Loan" , "View Request"].map((text, index) => (
+            <ListItem key={text} disablePadding sx={{ my: 2 }}>
+              <ListItemButton>
                 <ListItemIcon>
                   {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
                 </ListItemIcon>
-                <ListItemText primary={key} />
+                <ListItemText primary={text} />
               </ListItemButton>
-             
             </ListItem>
-          )) : null}          
+          ))}
         </List>
       </Box>
     </Drawer>
